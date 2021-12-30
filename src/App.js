af8d6a1e149908c9a2233834c5f90b5a65cb914b
Ubuntu <sushi314@hotmail.com>
2021-12-30 10:40:27 -0500

    init change

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import './App.css';
+import Todos from './components/Todos.js'
+
+class App extends Component {
+//function App() {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the recycle',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  render() {
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    )  
+  }
+}
+
+export default App;
