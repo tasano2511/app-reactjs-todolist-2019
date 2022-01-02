20f94030775254ccd31c52178121ead44373a74d
Ubuntu <sushi314@hotmail.com>
2022-01-02 13:13:15 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react'
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/layout/Header'
 import Todos from './components/Todos.js'
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
+import { v4 as uuidv4 } from 'uuid'
+
+import './App.css';
 
 class App extends Component {
 //function App() {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Take out the recycle',
         completed: false
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Dinner with wife',
         completed: true
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -42,13 +48,39 @@ class App extends Component {
       todo => todo.id !== id)] }) // spread operator copies everything in there
   }
 
+  // Add Todo
+  addTodo = (title) => {
+    //console.log(title)
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
   render() {
     console.log(this.state.todos)
+    // In react-router-dom v6 the Route components no longer have render or component props,
+    // Use element props
     return (
-      <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} 
-        delTodo={this.delTodo} />
-      </div>
+      <Router>
+        <Fragment>
+          <Header />
+          <Routes>
+            <Route className="App" path='/' element={
+              <Fragment>            
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete} 
+                delTodo={this.delTodo} />
+              </Fragment> }
+            />
+            <Route className="App" path='/about' 
+              element={ <About /> }
+            />
+          </Routes>
+        </Fragment>
+      </Router>
     )  
   }
 }
